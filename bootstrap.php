75193842a88fc6f774147b7347efb45e6a99cdae
vishalkakadiya <vishalkakadiya123@gmail.com>
2017-03-06 16:35:58 +0530

    Change path of plugin file

--- bootstrap.php
+++ bootstrap.php
@@ -1,23 +1,10 @@
 <?php
-/**
- * Bootstrap the plugin unit testing environment. Customize 'active_plugins'
- * setting below to point to your main plugin file.
- *
- * Requires WordPress Unit Tests (http://unit-test.svn.wordpress.org/trunk/).
- *
- * @package wordpress-plugin-tests
- */
-// Add this plugin to WordPress for activation so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'songs-mania/songs-mania.php' ),
-);
-// If the wordpress-tests repo location has been customized (and specified
-// with WP_TESTS_DIR), use that location. This will most commonly be the case
-// when configured for use with Travis CI.
-// Otherwise, we'll just assume that this plugin is installed in the WordPress
-// SVN external checkout configured in the wordpress-tests repo.
-if ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
-} else {
-	require dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/includes/bootstrap.php';
+
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+require_once $_tests_dir . '/includes/functions.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/src/songs-mania.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+require $_tests_dir . '/includes/bootstrap.php';
