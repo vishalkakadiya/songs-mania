fe020497e4fcf42b0771b2030ff623352cc03714
Vishal Kakadiya <vishalkakadiya123@gmail.com>
2018-08-11 14:52:31 +0530

    Revert "Develop"

--- includes/class-songs-mania-loader.php
+++ includes/class-songs-mania-loader.php
@@ -42,16 +42,7 @@ class Songs_Mania_Loader {
 	protected $filters;
 
 	/**
-	 * The array of shortcodes registered with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      array    $shortcodes    The shortcodes registered with WordPress to fire when the plugin loads.
-	 */
-	protected $shortcodes;
-
-	/**
-	 * Initialize the collections used to maintain the actions, filters and shortcodes.
+	 * Initialize the collections used to maintain the actions and filters.
 	 *
 	 * @since    1.0.0
 	 */
@@ -59,18 +50,18 @@ class Songs_Mania_Loader {
 
 		$this->actions = array();
 		$this->filters = array();
-		$this->shortcodes = array();
+
 	}
 
 	/**
 	 * Add a new action to the collection to be registered with WordPress.
 	 *
 	 * @since    1.0.0
-	 * @param    string $hook             The name of the WordPress action that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the action is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 * @param    int    $priority         Optional. he priority at which the function should be fired. Default is 10.
-	 * @param    int    $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+	 * @param    string               $hook             The name of the WordPress action that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the action is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int                  $priority         Optional. he priority at which the function should be fired. Default is 10.
+	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
 	 */
 	public function add_action( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->actions = $this->add( $this->actions, $hook, $component, $callback, $priority, $accepted_args );
@@ -80,11 +71,11 @@ class Songs_Mania_Loader {
 	 * Add a new filter to the collection to be registered with WordPress.
 	 *
 	 * @since    1.0.0
-	 * @param    string $hook             The name of the WordPress filter that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the filter is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 * @param    int    $priority         Optional. he priority at which the function should be fired. Default is 10.
-	 * @param    int    $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
+	 * @param    string               $hook             The name of the WordPress filter that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int                  $priority         Optional. he priority at which the function should be fired. Default is 10.
+	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
 	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
@@ -96,13 +87,13 @@ class Songs_Mania_Loader {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @param    array  $hooks            The collection of hooks that is being registered (that is, actions or filters).
-	 * @param    string $hook             The name of the WordPress filter that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the filter is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 * @param    int    $priority         The priority at which the function should be fired.
-	 * @param    int    $accepted_args    The number of arguments that should be passed to the $callback.
-	 * @return   array  $hooks            The collection of actions and filters registered with WordPress.
+	 * @param    array                $hooks            The collection of hooks that is being registered (that is, actions or filters).
+	 * @param    string               $hook             The name of the WordPress filter that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int                  $priority         The priority at which the function should be fired.
+	 * @param    int                  $accepted_args    The number of arguments that should be passed to the $callback.
+	 * @return   array                                  The collection of actions and filters registered with WordPress.
 	 */
 	private function add( $hooks, $hook, $component, $callback, $priority, $accepted_args ) {
 
@@ -111,7 +102,7 @@ class Songs_Mania_Loader {
 			'component'     => $component,
 			'callback'      => $callback,
 			'priority'      => $priority,
-			'accepted_args' => $accepted_args,
+			'accepted_args' => $accepted_args
 		);
 
 		return $hooks;
@@ -119,46 +110,7 @@ class Songs_Mania_Loader {
 	}
 
 	/**
-	 * Add a new shortcode to the collection to be registered with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @param    string $hook             The name of the shortcode that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the shortcode is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 */
-	public function add_shortcode( $hook, $component, $callback ) {
-		$this->shortcodes = $this->add_shortcodes( $this->shortcodes, $hook, $component, $callback );
-	}
-
-	/**
-	 * A utility function that is used to register the shortcode hooks into a single
-	 * collection.
-	 *
-	 * @since    1.0.0
-	 *
-	 * @access   private
-	 *
-	 * @param    array  $hooks            The collection of shortcodes that is being registered.
-	 * @param    string $hook             The name of the shortcode that is being registered.
-	 * @param    object $component        A reference to the instance of the object on which the shortcode is defined.
-	 * @param    string $callback         The name of the function definition on the $component.
-	 *
-	 * @return   array  $hooks            The collection of shortcodes registered with WordPress.
-	 */
-	private function add_shortcodes( $hooks, $hook, $component, $callback ) {
-
-		$hooks[] = array(
-			'hook'          => $hook,
-			'component'     => $component,
-			'callback'      => $callback,
-		);
-
-		return $hooks;
-
-	}
-
-	/**
-	 * Register the filters, actions and shortcodes with WordPress.
+	 * Register the filters and actions with WordPress.
 	 *
 	 * @since    1.0.0
 	 */
@@ -170,10 +122,6 @@ class Songs_Mania_Loader {
 
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
-		}
-
-		foreach ( $this->shortcodes as $hook ) {
-			add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
 		}
 
 	}
